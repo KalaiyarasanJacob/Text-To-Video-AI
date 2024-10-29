50ed6fcea8ec1c7f18f2fa431ea2b91472adfc25
KalaiyarasanJacob <kalaiyarasanjacob@gmail.com>
2024-10-29 22:48:04 +0530

    Update script_generator.py

--- utility/script/script_generator.py
+++ utility/script/script_generator.py
@@ -2,16 +2,9 @@ import os
 from openai import OpenAI
 import json
 
-if len(os.environ.get("GROQ_API_KEY")) > 30:
-    from groq import Groq
-    model = "mixtral-8x7b-32768"
-    client = Groq(
-        api_key=os.environ.get("GROQ_API_KEY"),
-        )
-else:
-    OPENAI_API_KEY = os.getenv('OPENAI_KEY')
-    model = "GPT-4o mini"
-    client = OpenAI(api_key=OPENAI_API_KEY)
+model = "gpt-3.5-turbo"
+OPENAI_API_KEY = os.environ.get('OPENAI_KEY')
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 def generate_script(topic):
     prompt = (
